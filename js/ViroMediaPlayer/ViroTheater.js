c25386bea059c362546ed8ed129bfca8327375a4
beelarr <bryonl@me.com>
2018-01-01 17:02:42 -0600

    Build of theater

---
+++ js/ViroMediaPlayer/ViroTheater.js
@@ -0,0 +1,221 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import { StyleSheet } from 'react-native';
+
+import {
+    AppRegistry,
+    ViroScene,
+    ViroVideo,
+    ViroMaterials,
+    Viro360Image,
+    ViroButton,
+    ViroImage,
+    ViroNode,
+    ViroAnimations,
+} from 'react-viro';
+
+var createReactClass = require('create-react-class');
+
+var buttonSize = 0.25;
+var VIDEO_REF = 'videoref';
+var VideoControlRef = "VideoControlRef";
+
+var videos = [
+    {uri:'https://s3-us-west-2.amazonaws.com/viro/Assets/Viro_Media_Video.mp4'},
+    {uri:'https://s3-us-west-2.amazonaws.com/viro/Assets/ProductVideo.mp4'},
+];
+
+var ViroTheater = createReactClass({
+    getInitalState() {
+        return {
+            videoControlsAnimation:"fadeIn",
+            videoPaused: false,
+            loopVideo: true,
+            videoIndex: 0,
+            runAnimation: false,
+        }
+    },
+    render() {
+        return (
+            <ViroScene
+                onClick={this._onVideoTapped}
+                reticleEnabled={this.state.videoControlsAnimation=="fadeIn"}>
+                <Viro360Image source={require('./res/dark_theatre.jpg')} />
+                <ViroVideo
+                    ref={VIDEO_REF}
+                    source={videos[this.state.videoIndex]}
+                    volume={1.0}
+                    position{[0, 3.9, -45]}
+                    scale={[44,22,1]}
+                    loop={this.state.loopVideo}
+                    paused={this.state.videoPaused} />
+
+                {this._renderVideoControl()}
+
+            </ViroScene>
+        )
+    },
+
+    _onVideoTapped ()  {
+        var videoControlAnimationState = this.state.videoControlsAnimation;
+        if (videoControlAnimationState=="fadeIn") {
+            videoControlAnimationState="fadeOut"
+        }else {
+            videoControlAnimationState="fadeIn";
+        }
+
+        this.setState({
+            videoControlsAnimation:videoControlAnimationState,
+            runAnimation:true,
+        });
+    },
+
+    _renderVideoControl() {
+        return (
+            <ViroNode
+                position={[0, -0.8, 0]}
+                opacity={1.0}
+                animation={{
+                    name: this.state.videoControlsAnimation,
+                    run: this.state.runAnimation,
+                    loop:false
+                }}
+            >
+
+                <ViroImage
+                    scale={[1.4, 1.2, 1]}
+                    position={[0, -0.27, -2.1]}
+                    source={require('./res/player_controls_container.png')}
+                />
+
+                <ViroButton
+                    position={[-buttonSize-0.1, 0, -2]}
+                    scale={[1, 1, 1]}
+                    width={buttonSize}
+                    height={buttonSize}
+                    source={require('./res/previous.png')}
+                    gazeSource={require('./res/previous_hover.png')}
+                    tapSource={require('./res/previous_hover.png')}
+                    onClick={this._playPreviousVideo}
+                />
+
+                {this._renderPlayControl()}
+
+                <ViroButton
+                    position={[buttonSize+0.1, 0, -2]}
+                    scale={[1, 1, 1]}
+                    width={buttonSize}
+                    height={buttonSize}
+                    source={require('./res/skip.png')}
+                    gazeSource={require('./res/skip_hover.png')}
+                    tapSource={require('./res/skip_hover.png')}
+                    onClick={this._playNextVideo}
+                />
+
+                <ViroButton
+                    position={[-0.3, -0.4, -2]}
+                    scale={[1, 1, 1]}
+                    width={0.5}
+                    height={0.5}
+                    source={require('./res/icon_2D_hover.png')}
+                    gazeSource={require('./res/icon_2D_hover.png')}
+                    tapSource={require('./res/icon_2D_hover.png')}
+                />
+
+                <ViroButton
+                    position={[0.3, -0.4, -2]}
+                    scale={[1, 1, 1]}
+                    width={0.5}
+                    height={0.5}
+                    source={require('./res/icon_360.png')}
+                    gazeSource={require('./res/icon_360_hover.png')}
+                    tapSouce={require('./res/icon_360_hover.png')}
+                    onClick={this._launchTheaterScene}
+
+                />
+
+            </ViroNode>
+        );
+    },
+
+    _renderPlayControl() {
+        if (this.state.videoPaused) {
+            return (
+                <ViroButton
+                    position={[0, 0, -2]}
+                    scale={[1, 1, 1]}
+                    width={buttonSize}
+                    height={buttonSize}
+                    source={require('./res/pause.png')}
+                    gazeSource={require('./res/play_hover.png')}
+                    tapSource={require('./res/play_hover.png')}
+                    onClick={this._togglePauseVideo}
+                />
+
+            );
+        } else {
+            return (
+                <ViroButton
+                    position={[0,0,-2]}
+                    scale={[1, 1, 1]}
+                    width={buttonSize}
+                    height={buttonSize}
+                    source={require('./res/pause.png')}
+                    gazeSource={require('./res/pause_hover.png')}
+                    tapSource={require('./res/pause_hover.png')}
+                    onClick={this._togglePauseVideo}
+
+                />
+            );
+        }
+    },
+
+    _launchTheaterScene(){
+        this.props.sceneNavigator.jump("Viro360Theater", {scene:require('./Viro360Theater')});
+    },
+
+    _togglePauseVideo() {
+        this.setState({
+            videoPaused: !this.state.videoPaused,
+        })
+    },
+
+    _playPreviousVideo() {
+        var currentVideo = this.state.videoIndex;
+        if (currentVideo - 1 > -1) {
+            this.setState({
+                videoIndex: (currentVideo -1),
+                videoPaused: false,
+            });
+        }
+    },
+
+    _playNextVideo() {
+        var currentVideo = this.state.videoIndex;
+        if (currentVideo + 1 < videos.length) {
+            this.setState({
+                videoIndex: (currentVideo + 1),
+                videoPaused: false
+            });
+        }
+    },
+
+});
+
+
+ViroAnimations.registerAnimations({
+    fadeOut:{properties:{opacity: 0.0}, duration: 500},
+    fadeIn:{properties:{opactiy: 1.0}, duration: 500},
+});
+
+ViroMaterials.createMaterials({
+    opaqueWhite: {
+        shininess: 2.0,
+        lightingModel: "Lambert",
+        diffuseColor: " #FFFFFF"
+    },
+});
+
+module.exports = ViroTheater;
