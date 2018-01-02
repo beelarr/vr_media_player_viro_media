1daf20f5d005d7b49a67d0f9c23bf9b54c68bc64
beelarr <bryonl@me.com>
2018-01-02 08:39:33 -0600

    Add imports and create class to 360 video

---
+++ js/ViroMediaPlayer/Viro360Theater.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+import {
+    AppRegistry,
+    ViroScene,
+    ViroVideo,
+    ViroSceneNavigator,
+    ViroMaterials,
+    Viro360Video,
+    ViroButton,
+    ViroImage,
+    ViroNode,
+    ViroAnimations,
+} from 'react-viro';
+
+var createReactClass = require('create-react-class');
+
+var buttonSize = 0.25;
+var VIDEO_REF = 'videoref';
+
